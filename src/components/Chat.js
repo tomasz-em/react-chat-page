7c0f6a009ddcc8b633a056a5088646410a296efb
Tomasz-eM <56788219+tomasz-em@users.noreply.github.com>
2020-06-14 21:01:29 +0200

    separating <Chat> from <App> and adding the SASS preprocessor
    * <App> is left as the main component
    * created "components" folder for storing components
    * adding the SASS proprocessor - "node-sass"
    * saved the address of the chat server in the component variable <Chat>

---
+++ src/components/Chat.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import * as io from 'socket.io-client';
+
+import './Chat.css';
+
+class Chat extends React.Component {
+  socket = null;
+
+  constructor() {
+    super();
+
+    this.chatServerUrl = 'https://socket-chat-server-zbqlbrimfj.now.sh';   // możliwy do użycia też 'https://chat-server.fbg.pl' 
+
+    this.state = {
+      authorId: 'Wojtek eM',
+      messages: [
+        // { text: string, authorId: number }
+      ]
+    };
+  }
+
+  componentWillMount() {
+    this.socket = io.connect( this.chatServerUrl, {
+      transports: ['websocket'],
+      reconnection: true
+    });
+
+    this.socket.on('chat message', message => {
+      this.state.messages.push(message);
+      this.setState({ messages: this.state.messages });
+    });
+  }
+
+  sendMessage = () => {
+    const text = this.refs.textarea.value.trim();
+    if (text) {
+      const message = { text, authorId: this.state.authorId };
+      this.socket.emit('chat message', message);
+      this.refs.textarea.value = '';
+    }
+  };
+
+  handleEnterPress = event => {
+    if (event.keyCode === 13) {
+      this.sendMessage();
+    }
+  };
+
+  render() {
+    return (
+      <section className="wrapper">
+        <div className="chat">
+          <header>Contact us!</header>
+          <section>
+            {this.state.messages.map(message => (
+              <div key={message.id} className="message">
+                {message.authorId}:{' '}
+                {message.authorId === this.state.authorId ? (
+                  <span>{message.text}</span>
+                ) : (
+                    message.text
+                  )}
+              </div>
+            ))}
+          </section>
+          <footer>
+            <textarea
+              ref="textarea"
+              className="textarea"
+              onKeyUp={this.handleEnterPress}
+              placeholder="Type a message here and press enter..."
+            />
+            <button onClick={this.sendMessage} className="button">
+              Send
+            </button>
+          </footer>
+        </div>
+      </section>
+    );
+  }
+}
+
+export { Chat };
