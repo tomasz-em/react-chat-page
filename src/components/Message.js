528d0a21fb527976c52f6ee0171458c5dfc4f7d1
Tomasz-eM <56788219+tomasz-em@users.noreply.github.com>
2020-06-20 12:04:29 +0200

    a better way to display each message
    * improved the view of the displayed message
      - a complex structure based on block elements was introduced
      - quite legible elements despite grouping the nickname/name and time
      - font slightly increased for all message content and darkened
      - additional distinguishing feature for your own messages (border and background color)
      - ...the message area should respond to a click, now only shown this on the ": hover" state of the cursor
      - tested appearance on long and short messages and on long nicknames in both screen widths
      - added slighty rounding for main elements
    * improved information/legend in <Chat>
      - the area slightly shifted to the left to be able to operate the vertical slider at a large amount of volume
      - changed for two chat window widths respectively
    * corrected problems in CSS:
      - "word-break: break-all" handles transfer to a new line
      - ...applies to too long content, not separated by a space
      - ...changes have been made to <Chat> and <Message>, wherever too long content may be received
      - fixed in IE the problem of squeezing the delete button when the message area is too wide

--- src/components/Message.js
+++ src/components/Message.js
@@ -4,38 +4,40 @@ import './Message.scss';
 
 const Message = ( props ) => { // ({ message, authorId })
 
+  let { message, authorId } = props;  // destrukturyzacja teraz, zamiast w atrybutach funkcji - potzrebny dostęp do "props"
+
   function convertTimestampToHMSString ( timestamp ) {
-    const convertedDate = new Date( timestamp ); // tu to "prawdziwy" timestamp, czyli wyrażony w milisekundach (nie trzeba mnożyć przez 1k)
-    // return convertedDate.toUTCString() + " " + convertedDate.toLocaleString();  // po prostu godzina z dopełenieniem zerowym (jak w lokalnych ustawieniach komputera/przeglądarki) 
+    const convertedTime = new Date( timestamp ); // tu to "prawdziwy" timestamp, czyli wyrażony w milisekundach (nie trzeba mnożyć przez 1k)
+    // po prostu czas z dopełenieniem zerowym elementów (jak w lokalnych ustawieniach komputera/przeglądarki) 
     // zawsze dodaje zero przed "tekstem czasu", by potem dwa ostatnie znaki napisu tylko wyświetlić
-    return ( "0" + convertedDate.getHours() ).substr(-2) + ':' + ( "0" + convertedDate.getMinutes() ).substr(-2) + ':' + ( "0" + convertedDate.getSeconds() ).substr(-2);
+    return ( "0" + convertedTime.getHours() ).substr(-2) + ':' + ( "0" + convertedTime.getMinutes() ).substr(-2) + ':' + ( "0" + convertedTime.getSeconds() ).substr(-2);
   }
 
   let handleClickOnX = (event) => { // lokalna obsług akliknięcia.. ale wywoła metodę z komponentu rodzica dla wszanych parametrów wywołania
     if ( props.onClick ) {
       console.log("ZDARZENIE_CLICK_MESSAGE", event);
-      props.onClick( props.message ); // wyślij zwrotnie obiekt konkretnej wiadomości (cztery atrybuty)
+      props.onClick( message ); // wyślij zwrotnie obiekt konkretnej wiadomości (cztery atrybuty)
     }
    }
 
   return (
-    <div className={ props.message.authorId === props.authorId ? "message my-message" : "message" }>
-      <span className="delete-me" title="Usuń wiadomość" onClick={ handleClickOnX } >&times;</span>
-      <span className="time">{ convertTimestampToHMSString( props.message.timestamp ) } </span>
-      { props.message.authorId }:{' '}
-      { props.message.authorId === props.authorId ? (
-      <span className="my-post-text">{ props.message.text }</span>
-      ) : (
-          props.message.text
-      )}
-    </div>
-
+    <article className="message" >
+      <section>
+        <div className="delete-me" title="Usuń wiadomość" onClick={ handleClickOnX } >
+            &times;
+        </div>
+        <div className={ message.authorId === authorId ? "message-container my-message" : "message-container" } >
+          <p className="author">
+            <strong>{ message.authorId }</strong> o&nbsp;
+            <strong className="time">{ convertTimestampToHMSString( message.timestamp ) }</strong>
+          </p>
+          <p className={ message.authorId === authorId ? "my-post-text" : "" } >
+            { message.text }
+          </p>
+        </div>
+      </section>
+    </article>
   )
-} // MEssages-END
+} // Message-END
 
 export { Message };
-
-{/* <form>
-<BookTitle onTitleChange={handleTitleChange} title={title} />
-</form>
- */}
